bd9534d9667e7a0cbc85ec5ffbb6b11cc05f8aa8
karadenizemirr <karadenizemirr@gmail.com>
2022-12-04 12:20:12 +0300

    Update - 1

--- modules/scraper.py
+++ modules/scraper.py
@@ -19,6 +19,7 @@ class Scraper:
         options = webdriver.ChromeOptions() 
         options.add_argument("start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option('useAutomationExtension', False)
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
